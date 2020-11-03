e9615825e5638927db1faccdbb99aff140f9228b
Jonah Keller <jkeller10@stuy.edu>
2020-11-02 20:45:20 -0500

    I implemented up to set; i'm going to check if it compiles now

---
+++ SuperArray.java
@@ -0,0 +1,45 @@
+public class SuperArray{
+  // Instance Variables
+  private String[] data;
+  private int size; // the current size
+
+
+  // Constructor
+  public SuperArray()
+  {
+    size = 0 ;
+    data = new String [10];
+
+  }
+
+  public int size()
+  {
+    return size;
+  }
+
+  public boolean add(String element)
+  {
+    data[size] = element;
+
+    size++;
+
+    return true;
+  }
+
+  public String get(int index)
+  {
+    return data[index];
+  }
+
+  public String set(int index, String element)
+  {
+
+    String Old_Value = data[index];
+
+    data[index] = element;
+
+    return Old_Value;
+  }
+
+  
+}
