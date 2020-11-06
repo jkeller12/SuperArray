2cb6a7a9ee0ab70494164b828907542b66fbae83
Jonah Keller <jkeller10@stuy.edu>
2020-11-06 12:19:05 -0500

    I implemented Zip and it works

--- Demo.java
+++ Demo.java
@@ -28,6 +28,29 @@ public class Demo{
     return Return_SuperArray;
   }
 
+  public static SuperArray zip(SuperArray a, SuperArray b)
+  {
+    // Takes 2 superarrays and merges them
+    //[a0, b0, a1, b1, a2, b2,...]
+    // When there sizes are different, just fill in the resizeTester
+    SuperArray ReturnArray = new SuperArray();
+    int Smaller_SIZE = (a.size()> b.size()) ? b.size(): a.size();
+    int Larger_SIZE = (a.size()> b.size()) ? a.size(): b.size();
+    SuperArray Larger = (a.size()> b.size()) ? a: b;
+    for (int i = 0; i < Smaller_SIZE; i++)
+    {
+      ReturnArray.add(a.get(i));
+      ReturnArray.add(b.get(i));
+    }
+
+    for (int i = Smaller_SIZE; i < Larger_SIZE; i++)
+    {
+      ReturnArray.add(Larger.get(i));
+    }
+
+    return ReturnArray;
+  }
+
 
 
 
@@ -43,8 +66,13 @@ public class Demo{
       //removeDuplicates(words);
       SuperArray WORDSB = new SuperArray();
       //grouped to save vertical space
-      WORDSB.add("kani");   WORDSB.add("uni");     WORDSB.add("ebi");
-      WORDSB.add("daba shouldn't be here");
+      WORDSB.add("B0");   WORDSB.add("B1");     WORDSB.add("B2");
+    //  WORDSB.add("daba shouldn't be here");
+
+      SuperArray WORDSB1 = new SuperArray();
+      //grouped to save vertical space
+      WORDSB1.add("A0");   WORDSB1.add("A1");     WORDSB1.add("A2");
+      WORDSB1.add("A3");   WORDSB1.add("A4");
 
 
       SuperArray wordsA = new SuperArray();
@@ -59,6 +87,8 @@ public class Demo{
       System.out.println(words.equals(wordsA));
       System.out.println("Should be false");
       System.out.println(words.equals(WORDSB));
+      System.out.println("ZIP");
+      System.out.println(zip(WORDSB1, WORDSB));
 
 
       }
